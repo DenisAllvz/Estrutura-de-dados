96ba6549c9c6355eec174774d316dcdd3afb78a4
DenisAllvz <132939443+DenisAllvz@users.noreply.github.com>
2023-05-26 15:35:10 -0300

    Update stack.h

--- stack.h
+++ stack.h
@@ -2,11 +2,9 @@
 #define stack_h
 
 typedef struct stack Stack;
-
 Stack* new_stack(int size);
 void stack_push(Stack* s, int value);
 int stack_pop (Stack* s);
 void stack_print(Stack* s);
-
 
 #endif
